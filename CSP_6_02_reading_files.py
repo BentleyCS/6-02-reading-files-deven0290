e041311e1238139ac8cf3fe24df734a5190f5723
deven0290 <dmaiers26@bentleyschool.org>
2026-02-23 13:34:57 -0800

    Update CSP_6_02_reading_files.py

--- CSP_6_02_reading_files.py
+++ CSP_6_02_reading_files.py
@@ -1,20 +1,36 @@
-#You must make and submit your own test file and txt file with this assignment.
-
 def toString(fileName):
-    #This function returns the text from a given file.
-    #Any new lines are written as \n
+
     f = open(fileName)
     out = ""
     for line in f:
         out += line
     return out
-#print(toString("ExampleText.txt")=="Here is the text\ni am another line")
+
 
 def longestLine(fileName):
-    #Given a file return the longest line from within that file
-    pass
+
+    f = open(fileName)
+    longest = ""
+    for line in f:
+        line = line.rstrip("\n")  
+        if len(line) > len(longest):
+            longest = line
+    return longest
+
 
 def toBinary(fileName):
-    #Given a file that is only 0's and 1's return a list of the file broken into bytes.
-    #An example return might be ['01101001', '00101010', '1010']
-    pass
+   
+    f = open(fileName)
+    bits = ""
+    for line in f:
+        bits += line.strip()  
+
+    result = []
+    while len(bits) >= 8:
+        result.append(bits[:8])
+        bits = bits[8:]
+    if bits: 
+        result.append(bits)
+
+    return result
+
