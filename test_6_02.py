201ab71b70248bd6cb399eb2a70abec58bf383b3
deven0290 <dmaiers26@bentleyschool.org>
2026-02-23 14:22:49 -0800

    Update test_6_02.py

--- test_6_02.py
+++ test_6_02.py
@@ -1,10 +1,10 @@
-from file_functions import toString, longestLine, toBinary
+from CSP_6_02_reading_files import toString, longestLine, toBinary
 
 def test_toString():
     assert toString("myText.txt") != ""
 
 def test_longestLine():
-    assert longestLine("myText.txt") == "this line is the longest one in the file"
+    assert longestLine("myText.txt") == "eojfipoqwkljnfhiu3o1ijklmjn3fbui"
 
 def test_toBinary():
-    assert toBinary("myBinary.txt") == ['01101001', '00101010', '1010')
+    assert toBinary("myBinary.txt") == ['01101001', '00101010', '1010']
