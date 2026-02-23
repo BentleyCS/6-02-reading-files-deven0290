afbb7fd65a2effbee9538988ac6e41173f8fbb8c
deven0290 <dmaiers26@bentleyschool.org>
2026-02-23 13:40:18 -0800

    Create test_file_functions.py

---
+++ test_file_functions.py
@@ -0,0 +1,8 @@
+print("=== toString ===")
+print(repr(toString("myText.txt")))
+
+print("\n=== longestLine ===")
+print(longestLine("myText.txt"))
+
+print("\n=== toBinary ===")
+print(toBinary("myBinary.txt"))
